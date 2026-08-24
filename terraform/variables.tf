e7ef9c7e552ab1c68a3bc16c1d0c49d28bc07b93
peshweashwini <peshweashwini@gmail.com>
2026-08-24 16:53:45 +0530

    Update variables.tf
    Creating second variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "location" {
   type        = string
   default     = "East US"
 }
+
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  type        = string
+  default     = "rg-azure-devops-poc"
+}
